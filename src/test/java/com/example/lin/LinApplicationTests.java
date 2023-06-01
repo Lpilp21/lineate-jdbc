3bc5a6451ea892926255b2e0189f833de0b03594
levani <lpilp21@freeuni.edu.ge>
2023-06-01 15:21:14 +0400

    Initial commit

---
+++ src/test/java/com/example/lin/LinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
